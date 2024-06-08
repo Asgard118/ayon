294f53737479570b1d1c376ab48a6f930d50d6c7
ArtemB118 <1evkakii1@gmail.com>
2024-06-08 13:03:45 +0500

    third commit

--- ayon_tools/ayon_func.py
+++ ayon_tools/ayon_func.py
@@ -20,7 +20,7 @@ headers = {
     "Content-Type": "application/json"
 }
      
-def get_bundles_list(bundle_name: str):
+def get_bundle(bundle_name: str):
     data = get_bundles()
     if 'bundles' in data:
         for bundle in data['bundles']:
@@ -28,10 +28,31 @@ def get_bundles_list(bundle_name: str):
                 return bundle['addons']
     return None
 
-def bundle_composition(name_bundle: str):
-    return get_bundles(name_bundle)
+def get_bundles():
+    return get_bundles()
 
-def studio_settings_addons(bundle_name: str, file_path: str = None):
+def create_bundle(new_bundl_url: str):
+
+    url = new_bundl_url
+
+    headers = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+    }
+    params = {
+        "token": "9720fa2803bb488a84877038bdc6dda5"
+    }
+
+    response = requests.post(url, params, headers=headers, json=data_bundle)
+
+    if response.status_code == 204:
+        print("create successfully.")
+    elif response.status_code == 422:
+        print("Validation Error:", response.json())
+    else:
+        print("Error:", response.status_code, response.text, response.content)
+
+def set_studio_addon_settings(bundle_name: str, file_path: str = None):
     data = get_addons_settings(bundle_name)
     if file_path is None:
         file_path = os.path.join(os.getcwd(), "settings.json")
@@ -39,22 +60,27 @@ def studio_settings_addons(bundle_name: str, file_path: str = None):
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-def get_project_settings_addons(project_name: str, *args: str):
+def get_project_addon_settings(project_name: str, *args: str):
     data = get_addons_project_settings(project_name)
-    if not args:
-        return data
     
     for arg in args:
-        if arg in data:
-            data = data[arg]
+      if arg in data:
+        data = data[arg]
     
+    return data
+
+def get_project_addons_settings(project_name: str)
+    data = get_addons_project_settings(project_name)
     return data
 
 def get_addons_setting(addon_name: str, name_project: str, name_budle: str):
     pro_data = get_addons_settings(project_name = name_project)
     studio_data = get_addons_settings(bundle_name = name_budle)
-    return pro_data, studio_data[addon_name]
+    return {
+      'project_settings': pro_data,
+      'studio_settings': studio_data[addon_name]
+    }
 
-def get_anatomy_project():
+def get_project_anatomy():
     data = get_project_anatomy_preset()
     return data
