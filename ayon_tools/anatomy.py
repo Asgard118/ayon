683497c0c1a7ae9187decfe488c38a9133ed98d3
ArtemB118 <1evkakii1@gmail.com>
2024-06-10 16:11:16 +0500

    end_anatomy

--- ayon_tools/anatomy.py
+++ ayon_tools/anatomy.py
@@ -25,10 +25,34 @@ def set_studio_anatomy_preset(preset_name: str, preset: dict):
         print("Preset updated successfully.")
     else:
         print(response.content)
-def create_studio_anatomy_preset(preset_name: str, preset: dict): ...
+def create_studio_anatomy_preset(preset_name: str, preset: dict):
+    """
+    Функция создает пресет анатомии
+    """
+    url = f"{auth.SERVER_URL}/api/anatomy/presets/{preset_name}"
+    response = requests.put(url=url, headers=auth.HEADERS, json=preset)
+    if response.status_code == 204:
+        print("Preset create successfully.")
+    else:
+        print(response.content)
 
 
 # project anatomy
-def get_project_anatomy(project_name: str) -> dict: ...
-def set_project_anatomy(project_name: str, anatomy: dict): ...
+def get_project_anatomy(project_name: str) -> dict:
+    """
+    Функция возвращает анатомия конкретного проекта
+    """
+    url = f"{auth.SERVER_URL}/api/projects/{project_name}/anatomy"
+    response = requests.get(url=url, headers=auth.HEADERS)
+    if response.status_code == 204:
+        return response
+    else:
+        print(response.content)
 
+def set_project_anatomy(project_name: str, anatomy: dict):
+    url = f"{auth.SERVER_URL}/api/projects/{project_name}/anatomy"
+    response = requests.post(url=url, headers=auth.HEADERS, json=anatomy)
+    if response.status_code == 204:
+        print("Anatomy updated successfully.")
+    else:
+        print(response.content)
