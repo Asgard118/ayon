b488dba5372053aee8271be973cce1477d7c71f9
ArtemB118 <1evkakii1@gmail.com>
2024-06-11 14:04:01 +0500

    added_bundles_func

--- ayon_tools/attributes.py
+++ ayon_tools/attributes.py
@@ -1,7 +1,15 @@
-
-# attributes
-def get_attributes() -> dict: ...
-def set_attributes(attributes: dict): ...
+from ayon_api import get_attributes_schema, set_attribute_config
+def get_attributes() -> dict:
+    """
+    Функция возвращает список атрибутов
+    """
+    data = get_attributes_schema()
+    return data
+def set_attributes(attributes: dict):
+    """
+    Функцию добавили в АПИ 8 числа, еще не понятно как она работает
+    """
+    set_attribute_config()
 
 
 def create_attribute(name: str, attr_type: str):
