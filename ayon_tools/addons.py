c7abbce82c1f43b16b22e61b0733c9aaad9155b2
ArtemB118 <1evkakii1@gmail.com>
2024-06-12 15:07:50 +0500

    added_set_addon_func

--- ayon_tools/addons.py
+++ ayon_tools/addons.py
@@ -1,4 +1,6 @@
 from ayon_api import get_addons_project_settings, get_addons_settings
+import requests
+from .auth import auth
 
 # studio addon settings
 def get_studio_settings(bundle_name: str) -> dict:
@@ -8,7 +10,12 @@ def get_studio_settings(bundle_name: str) -> dict:
     data = get_addons_settings(bundle_name)
     return data
 
-def set_studio_settings(addon_name: str, version: str, settings: dict): ...
+def set_studio_settings(addon_name: str, version: str, settings: dict):
+    """
+    Обновление конкретной версии аддона, по определенным настройкам
+    """
+    response = requests.post(url=f'{auth.SERVER_URL}/api/addons/{addon_name}/{version}/settings', headers=auth.HEADERS, json=settings)
+    return response.raise_for_status
 
 
 # project settings
@@ -18,5 +25,11 @@ def get_project_settings(project_name: str) -> dict:
     """
     data = get_addons_project_settings(project_name)
     return data
-def set_project_settings(project_name: str, settings: dict): ...
+def set_project_settings(addon_name: str, version: str, project_name: str, settings: dict):
+    """
+    Обновляет конкретную версию аддона проекта
+    """
+    response = requests.post(url=f'{auth.SERVER_URL}/api/addons/{addon_name}/{version}/settings/{project_name}', headers=auth.HEADERS, json=settings)
+    return response.raise_for_status
+
 
