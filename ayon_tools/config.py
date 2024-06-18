8fe62db5cc52cfc490fb9312920a5161c94296c7
paulwinex <paulwinex@gmail.com>
2024-06-18 10:45:46 +0300

    added config

--- ayon_tools/config.py
+++ ayon_tools/config.py
@@ -1,7 +1,20 @@
 import os
-import json
 from pathlib import Path
+import json
 
-STUDIOS = f"https://github.com/USERNAME/REPOSITORY/tree/{STUDIO_NAME}"
-DEFAULT_SETTINGS = "https://github.com/USERNAME/REPOSITORY/tree/DEFAULT"
-AYON_STUDIO_CONFIGS = "https://github.com/USERNAME/REPOSITORY/CONFIG"
+WORKDIR = Path(os.getenv("AYON_TOOLS_WORKDIR") or '~/.ayon_tools').expanduser()
+
+
+def load_config():
+    config_file = WORKDIR / "config.json"
+    if config_file.exists():
+        with config_file.open("r") as stream:
+            return json.load(stream)
+    return dict()
+
+
+conf = load_config()
+REPOSITORY_DIR = WORKDIR / "repository"
+REPOSITORY_URL = conf.get("configs_repository_url")
+STUDIO_CONFIG_DIR = WORKDIR / "studios"
+
