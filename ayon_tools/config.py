3f0377845ff758e2927a522783b02c13c55643c5
ArtemB118 <1evkakii1@gmail.com>
2024-06-18 12:44:38 +0500

    del_idea-again

--- ayon_tools/config.py
+++ ayon_tools/config.py
@@ -1,7 +1,2 @@
 import os
-import json
-from pathlib import Path
-
-STUDIOS = f"https://github.com/USERNAME/REPOSITORY/tree/{STUDIO_NAME}"
-DEFAULT_SETTINGS = "https://github.com/USERNAME/REPOSITORY/tree/DEFAULT"
-AYON_STUDIO_CONFIGS = "https://github.com/USERNAME/REPOSITORY/CONFIG"
+from pathlib
