5c69ea17ee5701d14d0c1d6008f96cf9dadfc761
paulwinex <paulwinex@gmail.com>
2024-07-07 17:18:36 +0300

    update compare_dicts func

--- ayon_tools/tools.py
+++ ayon_tools/tools.py
@@ -1,11 +1,8 @@
 import copy
 import inspect
-from ayon_tools.studio import StudioSettings
-from collections.abc import Mapping
-import json
 import importlib.util
 import sys
-import os
+import logging
 
 
 # check dicts is match
@@ -17,36 +14,49 @@ def compare_dicts(dict1, dict2, ignore_keys=None):
         if key in ignore_keys:
             continue
         if key not in dict2:
-            continue
+            logging.debug(f"Key '{key}' not found in second dictionary")
+            return False
         if isinstance(value, dict):
             if not isinstance(dict2[key], dict):
+                logging.debug(
+                    f"Value for key '{key}' is a dictionary in the first but not in the second"
+                )
                 return False
             if not compare_dicts(value, dict2[key], ignore_keys):
+                logging.debug(
+                    f"Values for key '{key}' in both dictionaries are not equal"
+                )
                 return False
         elif isinstance(value, list):
             if not isinstance(dict2[key], list):
+                logging.debug(
+                    f"Value for key '{key}' is a list in the first but not in the second"
+                )
                 return False
-            value_filtered = [
-                item for item in value if not any(k in item for k in ignore_keys)
-            ]
-            dict2_filtered = [
-                item for item in dict2[key] if not any(k in item for k in ignore_keys)
-            ]
-            if len(value_filtered) != len(dict2_filtered):
-                return False
-            for item in value_filtered:
-                if isinstance(item, dict):
+            if not value:
+                continue
+            item_types = set([type(i) for i in value])
+            if len(item_types) > 1:
+                logging.debug("Skip multi typed list WIP")
+                continue
+            item_type = item_types.pop()
+            if item_type is dict:
+                for d1 in value:
                     if not any(
-                        compare_dicts(item, x, ignore_keys) for x in dict2_filtered
+                        compare_dicts(d1, d2, ignore_keys=ignore_keys)
+                        for d2 in dict2[key]
                     ):
+                        logging.debug(f"Dict in key {key}: not found in second dict")
                         return False
-                else:
-                    if item not in dict2_filtered:
-                        return False
+            else:
+                if value != dict2[key]:
+                    logging.debug(
+                        f"Values for key '{key}' in both dictionaries are not equal"
+                    )
+                    return False
         else:
             if dict2[key] != value:
                 return False
-
     return True
 
 
@@ -78,118 +88,6 @@ def merge_dicts(d1, d2, skip_keys=None):
                 result[key] = value
 
     return result
-
-
-# def check_settings_match(studio: StudioSettings, **kwargs) -> bool:
-#     """
-#     Проверка соответствия настроек
-#     """
-#     # check anatomy
-#     is_match = True
-#     if not compare_dicts(studio.get_anatomy_preset(), studio.get_rep_anatomy()):
-#         is_match = False
-#     # check bundle
-#     remote_bundle = (
-#         studio.get_productions_bundle()
-#         if not kwargs.get("stage")
-#         else studio.get_staging_bundle()
-#     )
-#     # check addons
-#     remote_addons = remote_bundle["addons"]
-#     local_addons = studio.get_rep_bundle()["addons"]
-#     if not compare_dicts(remote_addons, local_addons):
-#         is_match = False
-#
-#     # check attributes
-#     if not compare_dicts(studio.get_attributes(), studio.get_rep_attributes()):
-#         is_match = False
-#
-#     # check projects
-#     # TODO
-#     return is_match
-#
-#
-# def update_dict_with_changes(original: dict, updates: dict) -> dict:
-#     """
-#     Рекурсивно обновляет словарь original значениями из словаря updates.
-#     """
-#     for key, value in updates.items():
-#         if (
-#             isinstance(value, dict)
-#             and key in original
-#             and isinstance(original[key], dict)
-#         ):
-#             update_dict_with_changes(original[key], value)
-#         else:
-#             original[key] = value
-#     return original
-#
-#
-# def merge_dict(dict1, dict2):
-#
-#     def is_dict(obj):
-#         return isinstance(obj, dict)
-#
-#     def is_list(obj):
-#         return isinstance(obj, list)
-#
-#     def merge_recursive(d1, d2):
-#         for key, value in d2.items():
-#             if key.endswith("+"):
-#                 original_key = key[:-1]
-#                 if original_key in d1:
-#                     if is_dict(d1[original_key]) and is_dict(value):
-#                         d1[original_key] = merge_recursive(d1[original_key], value)
-#                     elif is_list(d1[original_key]) and is_list(value):
-#                         d1[original_key].extend(value)
-#                     else:
-#                         raise TypeError(
-#                             f"Cannot merge {type(d1[original_key])} with {type(value)} for key {original_key}"
-#                         )
-#                 else:
-#                     d1[original_key] = value
-#             elif key.endswith("-"):
-#                 original_key = key[:-1]
-#                 if original_key in d1:
-#                     if is_list(d1[original_key]) and is_list(value):
-#                         d1[original_key] = [
-#                             item for item in d1[original_key] if item not in value
-#                         ]
-#                     elif is_dict(d1[original_key]) and is_dict(value):
-#                         for k in value.keys():
-#                             if k in d1[original_key]:
-#                                 del d1[original_key][k]
-#                     else:
-#                         raise TypeError(
-#                             f"Cannot perform difference on {type(d1[original_key])} with {type(value)} for key {original_key}"
-#                         )
-#             else:
-#                 if key in d1:
-#                     if type(d1[key]) != type(value):
-#                         raise TypeError(
-#                             f"Type mismatch for key {key}: {type(d1[key])} != {type(value)}"
-#                         )
-#                     if is_dict(d1[key]) and is_dict(value):
-#                         d1[key] = merge_recursive(d1[key], value)
-#                     else:
-#                         d1[key] = value
-#                 else:
-#                     d1[key] = value
-#         return d1
-#
-#     return merge_recursive(dict1.copy(), dict2)
-#
-#
-# def compare_dicts(dict1: dict, dict2: dict):
-#     for key in dict2:
-#         if key not in dict1:
-#             return False
-#         if isinstance(dict2[key], dict) and isinstance(dict1[key], dict):
-#             if not compare_dicts(dict1[key], dict2[key]):
-#                 return False
-#         elif dict1[key] != dict2[key]:
-#             return False
-#     return True
 
 
 def import_module_from_string(module_code, module_name):
