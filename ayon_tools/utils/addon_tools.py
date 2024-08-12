94a629c8bacecf1457cf49d3ab4b7fb266e4c06c
paulwinex <paulwinex@gmail.com>
2024-08-12 20:36:30 +0300

    update linting

--- ayon_tools/utils/addon_tools.py
+++ ayon_tools/utils/addon_tools.py
@@ -1,7 +1,9 @@
 from ayon_tools.studio import StudioSettings
 
+
 def get_addon_repo_path(addon: str, studio: StudioSettings, addon_ver: str):
     from ayon_tools.repository import Repository
+
     addon = studio.get_addon(addon)
     addon_url = addon.get_repository_url()
     addon_repo = Repository(addon_url)
