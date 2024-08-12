a0de0d7262406df695f93d129c73f6beb1791428
paulwinex <paulwinex@gmail.com>
2024-08-12 20:06:30 +0300

    update get_server_version func

--- ayon_tools/api/system.py
+++ ayon_tools/api/system.py
@@ -4,8 +4,12 @@ import ayon_api
 
 
 def restart(auth: Auth = default_auth):
-    response = requests.post(url=f'{auth.SERVER_URL}/api/system/restart', headers=auth.HEADERS)
+    response = requests.post(
+        url=f"{auth.SERVER_URL}/api/system/restart", headers=auth.HEADERS
+    )
     return response.raise_for_status()
 
-def get_server_info(auth: Auth = default_auth):
-    return ayon_api.get_server_version()
+
+def get_server_version(auth: Auth = default_auth):
+    with auth:
+        return ayon_api.get_server_version_tuple()
