f871d993b37a90e72645c63ac780536730e8528f
ArtemB118 <1evkakii1@gmail.com>
2024-08-12 17:49:38 +0500

    itils modded

--- ayon_tools/utils/addon_tools.py
+++ ayon_tools/utils/addon_tools.py
@@ -1,20 +1,10 @@
-import pygit2
-from ayon_tools.repository import Repository, repo
-from ayon_tools import exceptipns
+from ayon_tools.studio import StudioSettings
 
-def get_addon_repo_path(addon_name: str):
-    pa = repo.read_from_repo(f'addons/{addon_name}/info.yml', "main", type("NoneType"))
-    print(pa)
-    # repo_path = repo.get_file_content(f'addons/{addon_name}/info.yml')
-    # url = repo_path.get('url')
-    return pa
-
-def build():
-    ...
-
-def clone(addon_name: str):
-    repo_path = get_addon_repo_path(addon_name)
-    # url = repo_path.get('url')
-    # new_repo = Repository(url)
-
-clone('maya')
+def get_addon_repo_path(addon: str, studio: StudioSettings, addon_ver: str):
+    from ayon_tools.repository import Repository
+    addon = studio.get_addon(addon)
+    addon_url = addon.get_repository_url()
+    addon_repo = Repository(addon_url)
+    addon_repo.reload()
+    addon_repo.set_tag(addon_ver)
+    return addon_repo.workdir.as_posix()
