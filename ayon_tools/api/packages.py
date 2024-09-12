a318efee07d1edea4e20be472fbeb17af916f5db
paulwinex <paulwinex@gmail.com>
2024-09-12 16:46:47 +0300

    refactor api functions

--- ayon_tools/api/packages.py
+++ ayon_tools/api/packages.py
@@ -9,8 +9,6 @@ from ..exceptipns import DepPackageNotExists, DepPackageAlreadyInstalled
 from ayon_tools import tools
 
 
-
-# dependency packages
 def get_dep_packages(auth: Auth = default_auth) -> dict:
     """
     Возвращает dependency packages на сервере
@@ -34,16 +32,7 @@ def add_dep_package(filename, auth: Auth = default_auth):
     """
     Check if dep is not exist on server before use this function
     """
-    arch = next(DEP_PACKAGES_DIR.glob(f"{filename}.zip"), None)
-    if arch is None:
-        raise ValueError(
-            f"Dependency package {filename} not found in {DEP_PACKAGES_DIR}"
-        )
-    meta = next(DEP_PACKAGES_DIR.glob(f"{filename}.zip.json"), None)
-    if meta is None:
-        raise ValueError(
-            f"Dependency package {filename} not found in {DEP_PACKAGES_DIR}"
-        )
+    arch, meta = get_dep_package_files(filename)
 
     with open(meta, "r") as f:
         data = json.load(f)
@@ -82,8 +71,45 @@ def get_available_dep_package_list():
     return list(file.stem for file in DEP_PACKAGES_DIR.glob("*.zip"))
 
 
-def download_dep_package(package_name: str) -> str:
+def get_dep_package_files(filename: str) -> (str, str):
+    arch = next(DEP_PACKAGES_DIR.glob(f"{filename}.zip"), None)
+    if arch is None:
+        return download_dep_package(filename)
+    meta = next(DEP_PACKAGES_DIR.glob(f"{filename}.zip.json"), None)
+    if meta is None:
+        raise ValueError(
+            f"Dependency package {filename} not found in {DEP_PACKAGES_DIR}"
+        )
+    return arch, meta
+
+
+def download_dep_package(package_name: str) -> (str, str):
     """
     Download dependency package from storage
     """
+    raise NotImplementedError(
+        f"Downloading not ready. Put built dep packages to folder {DEP_PACKAGES_DIR} (.zip and .zip.json)"
+    )
     # TODO
+    # return arch, meta
+
+
+def get_package_meta_data(package_name: str):
+    meta = next(DEP_PACKAGES_DIR.glob(f"{package_name}.zip.json"), None)
+    if meta is None:
+        raise FileNotFoundError(f"Meta data for dep {package_name} not found")
+    with open(meta, "r") as f:
+        return json.load(f)
+
+
+def package_name_to_data(*package_names: str):
+    """
+    {
+      "platform_name": "filename.zip"
+    }
+    """
+    data = {}
+    for package_name in package_names:
+        meta = get_package_meta_data(package_name)
+        data[meta["platform"]] = meta["filename"]
+    return data
