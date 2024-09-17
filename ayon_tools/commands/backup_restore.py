6cfe06164a1900584ed076d640be18df33ce8c4c
evkaki <1evkakii1@gmail.com>
2024-09-17 23:30:57 +0500

    fix dump

--- ayon_tools/commands/backup_restore.py
+++ ayon_tools/commands/backup_restore.py
@@ -3,6 +3,9 @@ import logging
 import tempfile
 import os
 
+from deepdiff.helper import skipped
+
+from ayon_tools.api.bundles import BundleVariant
 from ayon_tools.api.installers import installer_exists
 from ayon_tools.studio import StudioSettings
 from pathlib import Path
@@ -15,25 +18,42 @@ def dump(studio: StudioSettings, path: str = None, **kwargs):
     path = Path(path or tempfile.mktemp(suffix=".json"))
     path.parent.mkdir(parents=True, exist_ok=True)
     # studio data
+    bundles = studio.get_bundles()
+    names = [x["name"] for x in bundles["bundles"]]
+    if BundleVariant.STAGING in names:
+        server_staging_bundle = studio.get_staging_bundle()
+    else:
+        server_staging_bundle = None
     data = dict(
-        installer=studio.get_installers(),
+        studio.get_installers(),
         server_anatomy=studio.get_default_anatomy_preset(),
         server_attributes=studio.get_attributes(),
-        server_staging_bundle=studio.get_staging_bundle(),
+        server_staging_bundle=server_staging_bundle,
         server_production_bundle=studio.get_productions_bundle(),
-        server_addons=studio.get_server_addons_settings(),
+        server_addons=studio.get_server_addons_settings(
+            BundleVariant.PRODUCTION, BundleVariant.PRODUCTION
+        ),
         projects={},
         dependency=studio.get_dep_packages(),
     )
     # projects data
     projects = studio.get_projects()
+    print("projects", projects)
     data["projects"] = {}
     for project in projects:
+        names = [x["name"] for x in bundles["bundles"]]
+        if BundleVariant.STAGING in names:
+            projects_settings_staging = studio.get_project_addons_settings(
+                project["name"], BundleVariant.STAGING, BundleVariant.STAGING
+            )
+        else:
+            projects_settings_staging = None
         data["projects"][project["name"]] = dict(
             anatomy=studio.get_project_anatomy(project["name"]),
-            settings=studio.get_project_addons_settings(project["name"]),
-            projects_settings_staging=studio.get_project_settings_for_status("staging", project["name"]),
-            projects_settings_production=studio.get_project_settings_for_status("production", project["name"])
+            projects_settings_staging=projects_settings_staging,
+            projects_settings_production=studio.get_project_addons_settings(
+                project["name"], BundleVariant.PRODUCTION, BundleVariant.PRODUCTION
+            ),
         )
     with open(path, "w") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
@@ -51,6 +71,8 @@ def restore(studio: StudioSettings, path: str, **kwargs):
 
     # installers
     for installer in data["installer"]["installers"]:
+        if studio.installer_exists(installer["filename"]):
+            continue
         studio.upload_installers(
             filename=installer["filename"],
             version=installer["version"],
@@ -75,8 +97,12 @@ def restore(studio: StudioSettings, path: str, **kwargs):
         studio.set_attributes_config(attribute_name=name, data=setting, scope=scope)
 
     # bundle
-    studio.update_bundle("staging", data["server_staging_bundle"])
-    studio.update_bundle("production", data["server_production_bundle"])
+    bundles = studio.get_bundles()
+    names = [x["name"] for x in bundles["bundles"]]
+    if BundleVariant.PRODUCTION in names:
+        studio.update_bundle("production", data["server_production_bundle"])
+    if BundleVariant.STAGING in names:
+        studio.update_bundle("staging", data["server_staging_bundle"])
 
     # addons
     for addon_name, addon_ver in data["server_production_bundle"]["addons"].items():
